25d7eaf3c41ed07a562284fdfa4c9bc58987e36f
MarkusHeinisch <markusheinisch83@googlemail.com>
2021-03-23 09:44:22 +0100

    bugfix: url error on refresh

--- webpack.config.js
+++ webpack.config.js
@@ -3,43 +3,47 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	mode: 'development',
-  entry: path.join(__dirname, 'src', 'index.jsx'),
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.(sa|c)ss$/,
-        exclude: /node_modules/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }
-    ]
-  },
-  resolve: {
-    extensions: [ '.js', '.jsx' ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: path.join(__dirname, 'src', 'index.html'),
-      filename: 'index.html'
-    })
-  ],
-  output: {
-	  path: path.resolve(__dirname, 'dist'),
-	  clean: true
-  },
-  devServer: {
-	writeToDisk: false
-  },
-  optimization: {
-    minimize: true,
-  }
+	entry: path.join(__dirname, 'src', 'index.jsx'),
+	module: {
+	rules: [
+		{
+		test: /\.(js|jsx)$/,
+		exclude: /node_modules/,
+		use: {
+			loader: 'babel-loader'
+		}
+		},
+		{
+		test: /\.(sa|c)ss$/,
+		exclude: /node_modules/,
+			use: [
+				'style-loader',
+				'css-loader'
+			]
+		}
+	]
+	},
+	resolve: {
+	extensions: [ '.js', '.jsx' ]
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: path.join(__dirname, 'src', 'index.html'),
+			filename: 'index.html'
+		})
+	],
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/',
+		clean: true
+	},
+	devServer: {
+		writeToDisk: false
+	},
+	optimization: {
+		minimize: true,
+	},
+	devServer: {
+		historyApiFallback: true,
+	}
 };
