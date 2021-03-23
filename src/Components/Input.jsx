cbb89a992320905af073a3c92b42903e5be54ecc
MarkusHeinisch <markusheinisch83@googlemail.com>
2021-03-23 16:38:31 +0100

    modified output of input component (label position)

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import Label from "./Label";
 import { buildClassName } from "./Util";
 
 export default function Input({ register, name, label, error, additional, ...rest }) {
 
-	const defaultClass = rest.type == "checkbox"
+	const isCheckbox = rest.type == "checkbox" ? true : false;
+	const defaultClass = isCheckbox
 		? "form-check-input"
 		: "form-control";
 
-	const className = buildClassName(defaultClass, rest.className, error ? "is-invalid" : "");
+	const classNameWrapper = "mb-3" + (isCheckbox ? " form-check" : "");
+	const classNameInput = buildClassName(defaultClass, rest.className, error ? "is-invalid" : "");
+
+	const output = {
+		label: <Label htmlFor={name} label={label} />,
+		input: <input id={name} name={name} ref={register} {...rest} className={classNameInput} />
+	};
 
 	return(
-		<div className="mb-3">
-			<Label htmlFor={name} label={label} />
-			<input id={name} name={name} ref={register} {...rest} className={className} />
+		<div className={classNameWrapper}>
+			{isCheckbox
+				? <>{output.input}{output.label}</>
+				: <>{output.label}{output.input}</>
+			}
 			{error && <div className="invalid-feedback">{error.message}</div>}
 			{additional && <>{additional}</>}
 		</div>
